5ec9ea19c79e503ef604e20015d643483360f827
yvonne <dayitsqq@gmail.com>
2023-09-08 15:43:24 +0800

    Initial commit

---
+++ FitMatch-Client/Controllers/TrainerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitMatch_Client.Controllers
+{
+    public class TrainerController : Controller
+    {
+        public IActionResult Match()
+        {
+            return View();
+        }
+        public IActionResult TrainerResume()
+        {
+            return View();
+        }
+    }
+}
