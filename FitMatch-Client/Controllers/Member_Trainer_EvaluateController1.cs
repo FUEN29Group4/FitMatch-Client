6c47104024bbecd87b2a0ec06c08684b931005fb
Air505 <fuen29wei@gmail.com>
2023-09-12 08:32:59 +0800

    新增會員課程紀錄、教練評價頁面

---
+++ FitMatch-Client/Controllers/Member_Trainer_EvaluateController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitMatch_Client.Controllers
+{
+    public class Member_Trainer_EvaluateController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
