038019408c8cc907606943db87d38c0c8b06d3ba
20001112 <gno2089922@gmail.com>
2023-09-12 16:43:55 +0800

    20230912 1643更新行事曆

---
+++ FitMatch-Client/Controllers/TrainerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitMatch_Client.Controllers
+{
+    public class TrainerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
