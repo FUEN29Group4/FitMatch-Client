c897e14d741fc3278289ee60751141ef30dacba2
xm90271 <xm90271@gmail.com>
2023-09-12 12:53:10 +0800

    註冊頁面修正

--- FitMatch-Client/Controllers/LoginController.cs
+++ FitMatch-Client/Controllers/LoginController.cs
@@ -10,23 +10,23 @@ namespace FitMatch_Client.Controllers
             return View();
         }
 
-        [HttpPost] // 用於處理表單提交
-        public IActionResult Login(string email, string password)
-        {
-            // 在這裡添加你的驗證邏輯，例如與資料庫比對
+        //[HttpPost] // 用於處理表單提交
+        //public IActionResult Login(string email, string password)
+        //{
+        //    // 在這裡添加你的驗證邏輯，例如與資料庫比對
 
-            bool someConditionForSuccess = true; // 假設這是驗證成功的條件
+        //    bool someConditionForSuccess = true; // 假設這是驗證成功的條件
 
-            if (someConditionForSuccess) // 登入成功
-            {
-                return RedirectToAction("Index", "Home"); // 重導向到 Home Controller 的 Index Action
-            }
-            else // 登入失敗
-            {
-                ViewBag.Error = "登入失敗，請再試一次。";
-                return View(); // 返回登入視圖並顯示錯誤訊息
-            }
-        }
+        //    if (someConditionForSuccess) // 登入成功
+        //    {
+        //        return RedirectToAction("Index", "Home"); // 重導向到 Home Controller 的 Index Action
+        //    }
+        //    else // 登入失敗
+        //    {
+        //        ViewBag.Error = "登入失敗，請再試一次。";
+        //        return View(); // 返回登入視圖並顯示錯誤訊息
+        //    }
+        //}
     }
 
 }
