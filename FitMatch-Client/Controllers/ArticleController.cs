9cd8fea729bb58723982174d60ed11d5df2ba66e
RoryFong <asas10407@gmail.com>
2023-09-18 16:42:37 +0800

    文章

---
+++ FitMatch-Client/Controllers/ArticleController.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace FitMatch_Client.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
