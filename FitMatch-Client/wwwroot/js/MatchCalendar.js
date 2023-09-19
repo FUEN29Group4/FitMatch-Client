79be73d223741aeb306d7c795fbe93f7271266e3
yvonne <110969170+yvonnehehe@users.noreply.github.com>
2023-09-20 02:37:54 +0800

    場館預約test

--- FitMatch-Client/wwwroot/js/MatchCalendar.js
+++ FitMatch-Client/wwwroot/js/MatchCalendar.js
@@ -9,30 +9,57 @@ const MatchCalendar = Vue.createApp({
         return {
             gymId: null,
             showCalendar: false,
-            events: [] // 用來存儲從外部 API 獲取的事件
+            events: [], // 用來存儲從外部 API 獲取的事件
+            calendar: null  // 用於存儲 FullCalendar 實例
+
         };
     },
+    mounted() {
+        // 這裡的this.gymId是您預設希望加載的場館ID。
+        // 如果您想在組件開始時為所有場館加載事件，則可能需要稍微修改此函數。
+        this.fetchEventsForgym(this.gymId);
+    },
     methods: {
+        formatTime(dateTimeString) { //轉換時間
+            let date = new Date(dateTimeString);
+            if (isNaN(date.getTime())) {
+                // 不是有效的日期字符串，返回原始值
+                return dateTimeString;
+            }
+            let year = date.getFullYear();
+            let month = date.getMonth() + 1;  // 月份從0開始，所以加1
+            let day = date.getDate();
+            let hour = date.getHours();
+            let minute = date.getMinutes();
+
+            // 如果月或日或小時或分鐘是單數字，添加前導0
+            month = month < 10 ? '0' + month : month;
+            day = day < 10 ? '0' + day : day;
+            hour = hour < 10 ? '0' + hour : hour;
+            minute = minute < 10 ? '0' + minute : minute;
+
+            return `${year}-${month}-${day} ${hour}:${minute}`;
+        },
         setgym(id) {
             this.gymId = id;
             this.showCalendar = true;
             this.fetchEventsForgym(id); // 獲取場館的事件
-            this.initializeCalendar();  // 初始化 FullCalendar
+            //this.initializeCalendar();  // 初始化 FullCalendar
 
         },
         fetchEventsForgym(gymid) {
             fetch(`https://localhost:7011/api/Reservation/MatchGym/${gymid}`)
-                .then(function (response) {
-                    console.log(gymid);
+                .then(response => {
+                    //console.log(gymid);
                     if (!response.ok) {
                         throw new Error("API請求失敗" + response);
                     }
                     return response.json();
                 })
                 .then(data => {
-                   
                     this.events = data.map(event => {
                         return {
+                            id: event.classId,
                             title: event.trainers[0].trainerName,
                             start: event.startTime,
                             end: event.endTime,
@@ -42,86 +69,73 @@ const MatchCalendar = Vue.createApp({
                             }
                         };
                     });
+                    this.initializeCalendar();  // 確保在數據更新後重新渲染日曆
                 });
         },
-        showMatch(id) {
-            // 在這裡處理當按鈕被點擊時的邏輯
-        },
-        initializeCalendar() {
-            $('#calendarModal').on('shown.bs.modal', function (e) {
-                const calendarEl = document.getElementById('calendar');
-                const calendar = new FullCalendar.Calendar(calendarEl, {
-                    initialView: 'dayGridMonth',
-                    // 設定初始視圖為週視圖
-                    initialView: 'timeGridWeek',
-                    selectable: true,
-                    allDaySlot: false,
-                    // 設定語言
-                    locale: 'zh-tw',
-                    // 設定時間範圍，早上9點到晚上10點
-                    slotMinTime: '09:00:00',
-                    slotMaxTime: '22:00:00',
-                    slotDuration: '01:00:00',  // 這裡設定每個時間格為一小時
-                    height: 650,
-                    titleFormat: { year: 'numeric' },  // 只顯示年分
-                    // 顯示上一週、下一週和今天按鈕
-                    headerToolbar: {
-                        left: 'prev',
-                        center: 'title',
-                        right: 'next today'
-                    },
-                    events: this.events,
-                    eventContent(arg) {
-                        let buttons = `
-        <button class="btn btn-available event-button" onclick="this.showEventDetails(${arg.event.id})">
-            ${arg.event.title}
-        </button>
-        <!-- 在這裡添加更多的按鈕 -->
-    `;
-                        let moreButton = `
-        <button class="btn more-button" onclick="this.toggleMore(this)">
-            ...
-        </button>
-    `;
-                        let html = `
-        <div class="event-container">
-            ${buttons}
-            ${moreButton}
-        </div>
-    `;
-                        return { html };
-                    }
-                    //eventClick: function (info) {
-                    //    if (!memberIdFromSession) {
-                    //        Swal.fire('提示', '尚未成為會員，無法進行預約', 'info');
-                    //        return;
-                    //    }
-                    //    let memberId = info.event.extendedProps.memberId;
-                    //    let courseStatus = info.event.extendedProps.courseStatus;
-                    //    if (!memberId && courseStatus === '進行中') {
-                    //        showModal(info.event);
-                    //    }
-                    //}
-                });
-                calendar.render();
-            }.bind(this));
-        },
-        toggleMore(button) { //處理 “…” 按鈕的點擊事件
-            let container = button.parentNode;
-            container.classList.toggle('expanded');
-        },
-        showEventDetails(eventId) {
+        async showEventDetails(eventId) {
+            eventId = Number(eventId); // 將eventId轉換為數字型別
             let event = this.events.find(e => e.id === eventId);
             if (event) {
-                Swal.fire({
-                    title: event.extendedProps.trainerName,
-                    text: `Class ID: ${event.classId}`,
-                    imageUrl: event.extendedProps.photo,
-                    imageWidth: 400,
-                    imageHeight: 200,
-                    imageAlt: 'Trainer photo',
-                });
+                // 格式化時間
+                const formattedStartTime = this.formatTime(event.start);
+                const formattedEndTime = this.formatTime(event.end);
+                // 獲取base64格式的圖片
+                try {
+                    Swal.fire({
+                        title: `教練: ${event.extendedProps.trainerName}`,
+                        html: `
+                  <img src="data:image/jpeg;base64,${event.extendedProps.photo}" alt="Trainer photo" style="width: 400px; height: 200px;">
+        <p>開始時間: ${formattedStartTime}</p>
+        <p>結束時間: ${formattedEndTime}</p>
+                `,
+                        showCancelButton: true,
+                        confirmButtonText: '確定預約',
+                        cancelButtonText: '取消',
+                        preConfirm: () => {
+                            // 這裡可以放入您要執行的「確定預約」動作，例如發送API請求等。
+                        }
+                    });
+                } catch (error) {
+                    console.error("Error converting image to Base64:", error);
+                }
             }
         },
+        initializeCalendar() {
+            const calendarEl = document.getElementById('calendar');
+            if (this.calendar) {
+                this.calendar.destroy();  // 如果日曆已經初始化，則先銷毀它
+            }
+            this.calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                // 設定初始視圖為週視圖
+                initialView: 'timeGridWeek',
+                selectable: true,
+                allDaySlot: false,
+                // 設定語言
+                locale: 'zh-tw',
+                // 設定時間範圍，早上9點到晚上10點
+                slotMinTime: '09:00:00',
+                slotMaxTime: '22:00:00',
+                slotDuration: '01:00:00',  // 這裡設定每個時間格為一小時
+                height: 650,
+                titleFormat: { year: 'numeric' },  // 只顯示年分
+                // 顯示上一週、下一週和今天按鈕
+                headerToolbar: {
+                    left: 'prev',
+                    center: 'title',
+                    right: 'next today'
+                },
+                events: this.events,
+                eventContent(arg) {
+                    let buttons = `<button class="btn btn-available event-button" >${arg.event.title}</button>`;
+                    let html = `<div class="event-container">${buttons}</div>`;
+                    return { html };
+                },
+                eventClick: (info) => {
+                    this.showEventDetails(info.event.id);
+                }
+            });
+            this.calendar.render();
+        }
     }
 }).mount("#MatchCalendar");
